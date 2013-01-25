4a986d8aa5349e9418e5eb26724d22c5478bb54a
George Hartzell <hartzell@alerce.com>
2013-01-24 17:23:18 -0800

    * updated pod docs to describe fancy Data::OptList-based arg   processing for keyword list.

--- lib/MooX/Role/WithDSL.pm
+++ lib/MooX/Role/WithDSL.pm
@@ -67,9 +67,26 @@ C<_build_dsl_keywords>.
 
 =requires _build_dsl_keywords
 
-A subrouting (used as the Moo{,se} builder for the L</dsl_keywords> attribute)
-that returns an array reference containing the list of methods that should be
-used as keywords in the DSL.
+A subroutine (used as the Moo{,se} builder for the L</dsl_keywords> attribute)
+that returns an array reference containing information about the methods that
+should be used as keywords in the DSL.
+
+In its canonical form the contents of the array reference are a series of array
+references containing method_name => { option_hash } pairs, e.g.
+
+  [ [ m1 => { as => kw1 } ], [ m2 => { as => kw2 ] ]
+
+However, as the contents of this array reference are processed with
+Data::OptList there is a great deal of flexibility.
+
+  [ qw( m1 m2 ), m4 => { as => kw4 } ]
+
+is equivalent to:
+
+  [ m1 => undef, m2 => undef, m4 => { as => kw4 } ]
+
+Options are optional, currently the only supported option is "as", to
+explicitly associate a keyword name with a method name.
 
 =cut
 
